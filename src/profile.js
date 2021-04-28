2be0490d13312a22423b13c89824c1a562b4a69d
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2021-04-28 19:51:56 +0300

    new

---
+++ src/profile.js
@@ -0,0 +1,26 @@
+const Profile = () => {
+  return (
+    <div id="article">
+    <div id="Kontent">
+	  <div id="Kontentblock" class="float-end">
+
+    <div class="Kontent-Block-img mb-4"></div>
+     
+    <div class="Kontent-Profil-Block float-start text-start">
+      <div class="Kontent-profile-img"></div>
+      <ul class="spisok-profile float-end list-group">
+        <li class="spisok-profile-li list-group-item bg-transparent">Login</li>
+        <li class="spisok-profile-li list-group-item bg-transparent">Status</li>
+        <li class="spisok-profile-li list-group-item bg-transparent">Adres</li>
+        <li class="spisok-profile-li list-group-item bg-transparent">Interesy</li>
+        <li class="spisok-profile-li list-group-item bg-transparent">Rabota</li>
+        <li class="spisok-profile-li list-group-item bg-transparent">Silka</li>
+      </ul>
+     </div>
+     </div>
+     </div>
+     </div>
+  );
+}
+
+export default Profile;
