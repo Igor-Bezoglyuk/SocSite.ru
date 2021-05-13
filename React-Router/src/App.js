bb1c41462f74ab45f8303344c6417d42a5fc1ac6
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2021-05-13 03:00:21 +0300

    new

---
+++ React-Router/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/header.js';
+import Page from './pages/Page.js';
+import Page2 from './pages/Page2.js';
+
+const App = () => {
+  return (
+  	<div className="App">
+  	<Header/>
+    </div>
+  );
+}
+
+export default App;
