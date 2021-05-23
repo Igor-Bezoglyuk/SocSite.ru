6f231def09eeaaf1c68a84aea91b227c5c77be79
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2021-05-23 19:20:25 +0300

    new

--- React-Router/src/pages/Massage.js
+++ React-Router/src/pages/Massage.js
@@ -1,32 +1,40 @@
 import '../App.css';
 import Avatar from '../img/avataricon.png';
+import {NavLink} from "react-router-dom";
 
-const Massage = () => {
-  return (
-  	<section id="MassageContent">
-    <div class="d-flex justify-content-center flex-wrap">
+const Users = () =>{
+  return(
     <div class="Massage-Blocks w-25 border-end border-2">
     <ul class="Massage-Spisok mb-5 ">
-     <li class="Massage-Spisok-Li"><a class="Massage-Spisok-silka" to="">Андрей</a></li>
-     <li class="Massage-Spisok-Li"><a class="Massage-Spisok-silka" to="">Дмитрий</a></li>
-     <li class="Massage-Spisok-Li"><a class="Massage-Spisok-silka" to="">Саша</a></li>
-     <li class="Massage-Spisok-Li"><a class="Massage-Spisok-silka" to="">Света</a></li>
-     <li class="Massage-Spisok-Li"><a class="Massage-Spisok-silka" to="">Валера</a></li>
-     <li class="Massage-Spisok-Li"><a class="Massage-Spisok-silka" to="">Надя</a></li>
-     <li class="Massage-Spisok-Li"><a class="Massage-Spisok-silka" to="">Коля</a></li>
-     <li class="Massage-Spisok-Li"><a class="Massage-Spisok-silka" to="">Катя</a></li>
+     <li class="Massage-Spisok-Li"><NavLink class="Massage-Spisok-silka" to="">Андрей</NavLink></li>
+     <li class="Massage-Spisok-Li"><NavLink class="Massage-Spisok-silka" to="">Дмитрий</NavLink></li>
+     <li class="Massage-Spisok-Li"><NavLink class="Massage-Spisok-silka" to="">Саша</NavLink></li>
+     <li class="Massage-Spisok-Li"><NavLink class="Massage-Spisok-silka" to="">Света</NavLink></li>
+     <li class="Massage-Spisok-Li"><NavLink class="Massage-Spisok-silka" to="">Валера</NavLink></li>
+     <li class="Massage-Spisok-Li"><NavLink class="Massage-Spisok-silka" to="">Надя</NavLink></li>
+     <li class="Massage-Spisok-Li"><NavLink class="Massage-Spisok-silka" to="">Коля</NavLink></li>
+     <li class="Massage-Spisok-Li"><NavLink class="Massage-Spisok-silka" to="">Катя</NavLink></li>
     </ul>
     </div>
-
+  );
+}
+const Mail = () =>{
+  return(
     <div class="Massage-Blocks w-50">
-
      <div class="Massage-Posts d-block mx-auto m-3 p-2">
      <img class="Massage-Posts-Img float-start me-3" src={Avatar} alt={"Avatar"}/>
      <h6 class="Massage-Posts-Title fs-5 fw-normal">Login Human</h6>
      <p class="Massage-Posts-Stroka fw-light">Сообщения от пользователя</p>
      </div>
-
     </div>
+  );
+}
+const Massage = () => {
+  return (
+  	<section id="MassageContent">
+    <div class="d-flex justify-content-center flex-wrap">
+    <Users/>
+    <Mail/>
     </div>
     </section>
   );
