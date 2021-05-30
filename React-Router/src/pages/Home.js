cc16997097c17b6dc0c224e491c6cf726c0d9c4d
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2021-05-30 21:02:49 +0300

    new

--- React-Router/src/pages/Home.js
+++ React-Router/src/pages/Home.js
@@ -1,12 +1,35 @@
+const Post = (props) =>{
+  return(
+    <div class="Create-Post float-start text-start">
+     <form class=" mt-4">
+        <div class="t-3">
+         <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+         <div id="emailHelp" class="form-text">{props.Posts}</div>
+       </div>
+       <div class="mb-3 form-check">
+         <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+         <label class="form-check-label" for="exampleCheck1">{props.checkbox}</label>
+       </div>
+       <button type="submit" class="btn btn-primary">{props.botton}</button>
+       </form>
+    </div>
+  );
+}
+const Home = (props) => {
+  let postCreate=[
+  { Posts:"Andrey"},
+  { checkbox:"Проверка"},
+  { botton:"Создать"},
+  ]
 
-const Home = () => {
+  let postMAP = postCreate.map(p => <Post Posts={p.Posts} checkbox={p.checkbox} botton={p.botton} />)
   return (
     <>
     <section id="Kontent">
 	  <div id="Kontentblock" class="float-start ps-5">
 
     <div class="Kontent-Block-img mb-4"></div>
-     
+
     <div class="Kontent-Profil-Block float-start text-start">
       <div class="Kontent-profile-img"></div>
       <ul class="spisok-profile float-end list-group">
@@ -18,19 +41,7 @@ const Home = () => {
         <li class="spisok-profile-li list-group-item bg-transparent">Silka</li>
       </ul>
      </div>
-     <div class="Create-Post float-start text-start">
-      <form class=" mt-4">
-         <div class="t-3">
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-          <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-        </div>
-        <div class="mb-3 form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
-     </div>
+     {postMAP}
      </div>
      </section>
      <section id="Kontent2">
--- React-Router/src/pages/Massage.js
+++ React-Router/src/pages/Massage.js
@@ -4,15 +4,14 @@ import {NavLink} from "react-router-dom";
 
 const Users = (props) =>{
   return(
-
     <ul class="Massage-Spisok mb-5 ">
      <li class="Massage-Spisok-Li"><NavLink class="Massage-Spisok-silka" to={"/Massage/"+ props.id}>{props.name}</NavLink></li>
     </ul>
   );
 }
 const Mail = (props) =>{
-  return(
 
+  return(
      <div class="Massage-Posts d-block mx-auto m-3 p-2">
      <img class="Massage-Posts-Img float-start me-3" src={Avatar} alt={"Avatar"}/>
      <h6 class="Massage-Posts-Title fs-5 fw-normal">{props.LogoName}</h6>
@@ -22,19 +21,32 @@ const Mail = (props) =>{
   );
 }
 const Massage = (props) => {
+  let dialogs=[
+  {id:1, name:"Andrey"},
+  {id:2, name:"Ivan"},
+  {id:3, name:"Oleg"},
+  {id:4, name:"Maxon"},
+  {id:5, name:"Olga"}
+  ]
+  let massage=[
+    {LogoName:"Andrey", Stroka:"Привет"},
+    {LogoName:"Ivan", Stroka:"Как дела?"},
+    {LogoName:"Oleg", Stroka:"Да норм по тихому?"},
+    {LogoName:"Maxon", Stroka:"Я немогу понять!"},
+    {LogoName:"Olga", Stroka:"Где компанента?"},
+  ]
+
+  let dialMAP = dialogs.map(d => <Users name={d.name} id={d.id}/>)
+  let massMAP = massage.map(m => <Mail LogoName={m.LogoNam} Stroka={m.Stroka}/>)
   return (
   	<section id="MassageContent">
     <div class="d-flex justify-content-center flex-wrap">
     <div class="Massage-Blocks w-25 border-end border-2">
-    <Users name="Andrey" id="1"/>
-    <Users name="Ivan" id="2"/>
-    <Users name="Oleg" id="3"/>
-    <Users name="Maxon" id="4"/>
-    <Users name="Olga" id="5"/>
+    {dialMAP}
     </div>
 
     <div class="Massage-Blocks w-50">
-    <Mail LogoName="h1" Stroka="вываыв"/>
+    {massMAP}
     </div>
     </div>
     </section>
