d877bf236085bf19a641d628b07317106d1251d4
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2021-06-06 21:00:02 +0300

    new

--- React-Router/src/data/inputdata.js
+++ React-Router/src/data/inputdata.js
@@ -1,1 +1,24 @@
+let data ={
+  dialogs:[
+  {id:1, name:"Andrey"},
+  {id:2, name:"Ivan"},
+  {id:3, name:"Oleg"},
+  {id:4, name:"Maxon"},
+  {id:5, name:"Olga"}
+],
+massage:[
+  {LogoName:"Andrey", Stroka:"Привет"},
+  {LogoName:"Ivan", Stroka:"Как дела?"},
+  {LogoName:"Oleg", Stroka:"Да норм по тихому?"},
+  {LogoName:"Maxon", Stroka:"Я немогу понять!"},
+  {LogoName:"Olga", Stroka:"Где компанента?"},
+],
+postCreate:[
+{ TitleLogin:"Andrey", StrokaSub:"Подробный важный, текст поста"},
+{ TitleLogin:"Andrey", StrokaSub:"Подробный важный, текст поста"},
+{ TitleLogin:"Andrey", StrokaSub:"Подробный важный, текст поста"},
+{ TitleLogin:"Andrey", StrokaSub:"Подробный важный, текст поста"}
+]
+}
 
+export default data;
