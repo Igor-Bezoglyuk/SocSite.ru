2d7844bb090ead18db1a3329791dba218529b50d
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2021-06-13 20:58:51 +0300

    new

--- React-Router/src/index.js
+++ React-Router/src/index.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import data from './data/inputdata.js';
 import reportWebVitals from './reportWebVitals';
+import {renderalways} from "./render";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App dataArray={data}/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+renderalways();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
