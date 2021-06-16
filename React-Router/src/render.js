7c5c8fbcad0cecf59c981f45ce3233c910433593
Igor-Bezoglyuk <78695421+Igor-Bezoglyuk@users.noreply.github.com>
2021-06-17 02:01:11 +0300

    new

--- React-Router/src/render.js
+++ React-Router/src/render.js
@@ -3,12 +3,13 @@ import App from './App';
 import ReactDOM from 'react-dom';
 import data from './data/inputdata.js';
 import {addPost1} from './data/inputdata.js';
+import {addPost2} from './data/inputdata.js';
 
 
 export let renderalways=()=>{
 ReactDOM.render(
   <React.StrictMode>
-    <App dataArray={data} addPost1={addPost1}/>
+    <App dataArray={data} addPost1={addPost1} addPost2={addPost2}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
